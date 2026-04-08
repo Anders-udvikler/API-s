8334fd443d58a0db261e02500f9d6f3354a55d73
LisePetculescu <lise.lili97gmail.com>
2026-04-08 23:07:37 +0200

    updated dto contract

--- SOAPAuthorAPI/Models/AuthorDto.cs
+++ SOAPAuthorAPI/Models/AuthorDto.cs
@@ -5,13 +5,13 @@ namespace Library.SoapApi.Models
     [DataContract(Name = "Author")]
     public class AuthorDto
     {
-        [DataMember(Name = "id", Order = 1)]
+        [DataMember(Name = "Id", Order = 1)]
         public int Id { get; set; }
 
-        [DataMember(Name = "name", Order = 2, IsRequired = true)]
+        [DataMember(Name = "Name", Order = 2, IsRequired = true)]
         public required string Name { get; set; }
 
-        [DataMember(Name = "surname", Order = 3, IsRequired = true)]
+        [DataMember(Name = "Surname", Order = 3, IsRequired = true)]
         public required string Surname { get; set; }
     }
 }
--- SOAPAuthorAPI/Models/PublishingCompanyDto.cs
+++ SOAPAuthorAPI/Models/PublishingCompanyDto.cs
@@ -5,10 +5,10 @@ namespace Library.SoapApi.Models
     [DataContract(Name = "PublishingCompany")]
     public class PublishingCompanyDto
     {
-        [DataMember(Name = "id", Order = 1)]
+        [DataMember(Name = "Id", Order = 1)]
         public int Id { get; set; }
 
-        [DataMember(Name = "name", Order = 2, IsRequired = true)]
+        [DataMember(Name = "Name", Order = 2, IsRequired = true)]
         public required string Name { get; set; }
     }
 }
